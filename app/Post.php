9b34addcb5f5742a69a8a6c191a5d3e4b4d49a56
Luca Sunseri <89150843+LucaSunseri@users.noreply.github.com>
2022-02-02 18:14:02 +0100

    Created seed PostTableSeeder

--- app/Post.php
+++ app/Post.php
@@ -3,8 +3,22 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Post extends Model
 {
-    //
+    static function gerateSlug($string)
+    {
+
+        $slug = Str::slug($string);
+        $original_slug = $slug;
+        $count = 1;
+
+        while (static::whereSlug($slug)->exists()) {
+
+            $slug = $original_slug . '-' . $count++;
+        }
+
+        return $slug;
+    }
 }
