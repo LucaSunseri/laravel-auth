538828f5526504dc0d34769eb47433d574e9ec6e
Luca Sunseri <89150843+LucaSunseri@users.noreply.github.com>
2022-02-02 18:31:59 +0100

    Created Crud and route Post

--- routes/web.php
+++ routes/web.php
@@ -25,4 +25,5 @@ Route::middleware('auth')
     ->name('admin.')
     ->group(function () {
         Route::get('/', 'HomeController@index')->name('index');
+        Route::resource('posts', 'PostController');
     });
